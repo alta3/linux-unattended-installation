e1e2fd3e533cc6303437078499de449c38ba55aa
seaneon <sean.r.barbour@gmail.com>
2021-02-09 22:55:01 +0000

    preseed compiles but not tested

--- templates/ubuntu/20.04-router/build-iso.sh
+++ templates/ubuntu/20.04-router/build-iso.sh
@@ -7,15 +7,7 @@ set -e
 : "${BIN_CPIO:=$(type -P gnucpio || type -P cpio)}"
 
 # get parameters
-SSH_PUBLIC_KEY_FILE=${1:-"$HOME/.ssh/id_rsa.pub"}
-TARGET_ISO=${2:-"`pwd`/ubuntu-20.04-lui-amd64.iso"}
-
-# check if ssh key exists
-if [ ! -f "$SSH_PUBLIC_KEY_FILE" ];
-then
-    echo "Error: public SSH key $SSH_PUBLIC_KEY_FILE not found!"
-    exit 1
-fi
+TARGET_ISO=${1:-"`pwd`/ubuntu-20.04-lui-amd64.iso"}
 
 # get directories
 CURRENT_DIR="`pwd`"
@@ -39,7 +31,6 @@ patch -p1 -i "$SCRIPT_DIR/custom/boot-menu.patch"
 cd "$TMP_INITRD_DIR"
 mkdir "./custom"
 cp "$SCRIPT_DIR/custom/preseed.cfg" "./preseed.cfg"
-cp "$SSH_PUBLIC_KEY_FILE" "./custom/userkey.pub"
 cp "$SCRIPT_DIR/custom/ssh-host-keygen.service" "./custom/ssh-host-keygen.service"
 
 # append assets to initrd image
@@ -52,7 +43,7 @@ cat "./initrd" | gzip -9c > "$TMP_DISC_DIR/initrd.gz"
 # build iso
 cd "$TMP_DISC_DIR"
 rm -r '[BOOT]'
-"$BIN_XORRISO" -as mkisofs -r -V "ubuntu_2004_netboot_unattended" -J -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -isohybrid-mbr "$SCRIPT_DIR/custom/isohdpfx.bin" -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -o "$TARGET_ISO" ./
+"$BIN_XORRISO" -as mkisofs -r -V "ubuntu_2004_netboot_unattended" -J -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -isohybrid-mbr "/usr/lib/ISOLINUX/isohdpfx.bin" -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -o "$TARGET_ISO" ./
 
 # go back to initial directory
 cd "$CURRENT_DIR"
